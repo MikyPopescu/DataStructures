d182e5d3e00111f4b93c8824816fc8eee47c4bb4
Popescu Mihaela <mihaellap42@gmail.com>
2019-06-18 22:39:04 +0300

    Citire

--- SimpleLinkedList/SimpleLinkedList/SimpleLinkedList.cpp
+++ SimpleLinkedList/SimpleLinkedList/SimpleLinkedList.cpp
@@ -1,19 +1,42 @@
-
 #include "pch.h"
 #include <iostream>
 #include<stdio.h>
 #include<malloc.h>
 struct Student {
-	char* name;
-	int noGrades;
-	int* grades;
+	char* nume;
+	int nrNote;
+	int* note;
 };
-struct node {
+struct nodls {
 	Student inf;
-	node* next;
+	nodls* next;
 };
 int main()
 {
+	int nrStudenti;
+	printf("\nNumarul de studenti: ");
+	scanf("%d", &nrStudenti);
+	
 	Student s;
+	nodls* cap = NULL;
+	char buffer[20];
+
+	for (int i = 0; i < nrStudenti; i++) {
+		printf("\nNume student: ");
+		scanf("%s", buffer);
+		s.nume = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(s.nume, buffer);
+		
+		printf("\nNr de note: ");
+		scanf("%d", &s.nrNote);
+
+		printf("\nNotele: ");
+		s.note = (int*)malloc(s.nrNote * sizeof(int));
+		for (int j = 0; j < s.nrNote; j++) {
+			scanf("%d", &s.note[i]);
+		}
+	}
+
+
 }
 
