465369e5f6e2e468aa243c556581b9597072546e
Popescu Mihaela <mihaellap42@gmail.com>
2019-06-18 22:25:26 +0300

    Added structs

--- SimpleLinkedList/SimpleLinkedList/SimpleLinkedList.cpp
+++ SimpleLinkedList/SimpleLinkedList/SimpleLinkedList.cpp
@@ -3,7 +3,16 @@
 #include <iostream>
 #include<stdio.h>
 #include<malloc.h>
-
+struct student {
+	int id;
+	char* name;
+	int noGrades;
+	int* grades;
+};
+struct node {
+	student inf;
+	node* next;
+};
 int main()
 {
     std::cout << "Hello World!\n"; 
