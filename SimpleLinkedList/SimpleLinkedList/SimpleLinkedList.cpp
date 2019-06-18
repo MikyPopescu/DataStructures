5bee29fff8c56fde9021e90f6035158143ce6e13
Popescu Mihaela <mihaellap42@gmail.com>
2019-06-18 23:20:43 +0300

    Insert in List

--- SimpleLinkedList/SimpleLinkedList/SimpleLinkedList.cpp
+++ SimpleLinkedList/SimpleLinkedList/SimpleLinkedList.cpp
@@ -11,6 +11,72 @@ struct nodls {
 	Student inf;
 	nodls* next;
 };
+
+//create a node
+nodls* creezNod(Student s) {
+	nodls* nou = (nodls*)malloc(sizeof(nodls));
+	nou->inf.nume = (char*)malloc((strlen(s.nume)+1)*sizeof(char));
+	strcpy(nou->inf.nume, s.nume);
+	nou->inf.nrNote = s.nrNote;
+	nou->inf.note = (int*)malloc(nou->inf.nrNote *sizeof(int));
+	for (int i = 0; i < nou->inf.nrNote; i++) {
+		nou->inf.note[i] = s.note[i];
+	}
+	nou->next = NULL;
+	return nou;
+}
+//insert a node at the beginning of a list
+nodls* inserareInceput(nodls* cap, Student s) {
+	nodls* nou = creezNod(s);
+	if (cap == NULL) {
+		cap = nou;
+	}
+	else {
+		nou->next = cap;
+		cap = nou;
+	}
+	return cap;
+}
+//insert a node at the end of a list
+nodls* inserareFinal(nodls* cap, Student s) {
+	nodls* nou = creezNod(s);
+	if (cap == NULL) {
+		cap = nou;
+	}
+	else {
+		nodls* temp = cap;
+		while(temp->next!=NULL){
+			temp = temp->next;
+		}
+		temp->next = nou;
+	}
+	return cap;
+}
+//crossing the elements of a list
+void traversare(nodls* cap) {
+	nodls* temp = cap;
+	while (temp!=NULL) {
+		printf("\nNume=%s,Nr Note=%d", temp->inf.nume, temp->inf.nrNote);
+		if (temp->inf.nrNote) {
+			printf("\nNotele studentului sunt: ");
+			for (int i = 0; i < temp->inf.nrNote; i++) {
+				printf("%d", temp->inf.note[i]);
+			}
+		}
+		temp = temp->next;
+	}
+}
+//free memory
+void dezalocare(nodls* cap) {
+	nodls* temp = cap;
+	while (temp!=NULL) {
+		nodls* temp2 = temp->next;
+		free(temp->inf.nume);
+		free(temp->inf.note);
+		free(temp);
+		temp = temp2;
+	}
+}
 int main()
 {
 	int nrStudenti;
@@ -35,8 +101,11 @@ int main()
 		for (int j = 0; j < s.nrNote; j++) {
 			scanf("%d", &s.note[i]);
 		}
+		//cap = inserareInceput(cap,s);
+		cap = inserareFinal(cap, s);
 	}
-
+	traversare(cap);
+	dezalocare(cap);
 
 }
 
