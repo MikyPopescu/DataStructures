041ddee70ba64a17573c097af99277faa2adb22e
Popescu Mihaela <mihaellap42@gmail.com>
2019-06-26 19:24:21 +0300

    conversii

--- DoubleLinkedList/DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList/DoubleLinkedList.cpp
@@ -1,6 +1,3 @@
-// DoubleLinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 #include<malloc.h>
@@ -14,6 +11,10 @@ struct nodld {
 	student inf;
 	nodld* next;
 	nodld* prev;
+};
+struct nodls {
+	student inf;
+	nodls* next;
 };
 nodld* creezNod(student s) {
 	nodld* nou = (nodld*)malloc(sizeof(nodld));
@@ -69,6 +70,86 @@ void dezalocare(nodld* cap) {
 		temp = temp2;
 	}
 }
+void stergeInceputLD(nodld** cap, nodld** coada) {
+	if (*cap != NULL) {
+		if (*cap == *coada) {
+			free((*cap)->inf.nume);
+			free((*cap)->inf.note);
+			free(*cap);
+			*cap = NULL;
+			*coada = NULL;
+		}
+		else {
+			nodld* temp = *cap;
+			*cap = temp->next;
+			free(temp->inf.nume);
+			free(temp->inf.note);
+			free(temp);
+			(*cap)->prev = NULL;
+		}
+	}
+}
+void conversieVector(nodld** cap, student** vect, int *nr) {
+	nodld* temp = *cap;
+	while (temp != NULL) {
+		student* copie = (student*)malloc((*nr) * sizeof(student));
+		for (int i = 0; i < *nr; i++) {
+			copie[i] = (*vect)[i];
+		}
+		*vect = (student*)malloc((*nr + 1) * sizeof(student));
+		(*nr)++;
+		for (int i = 0; i < *nr - 1; i++) {
+			(*vect)[i] = copie[i];
+		}
+		(*vect)[*nr - 1] = temp->inf;
+		nodld* temp2 = temp;
+		temp = temp->next;
+		free(temp2);
+		*cap = NULL;
+		
+	}
+}
+nodls* conversieLS(nodld** cap, nodld** coada) {
+	nodls* capLS = NULL;
+	while (*cap != NULL) {
+		nodls* nou = (nodls*)malloc(sizeof(nodls));
+		nou->inf.nume = (char*)malloc((strlen((*cap)->inf.nume) + 1) * sizeof(char));
+		
+		strcpy(nou->inf.nume, (*cap)->inf.nume);
+		nou->inf.nrNote = (*cap)->inf.nrNote;
+		nou->inf.note = (int*)malloc((*cap)->inf.nrNote * sizeof(int));
+		for (int i = 0; i < (*cap)->inf.nrNote; i++) {
+			nou->inf.note[i] = (*cap)->inf.note[i];
+		}
+		nou->next = NULL;
+		capLS = nou;
+		stergeInceputLD(cap, coada);
+		nodls* tempLS = capLS;
+		while (*cap) {
+			nodls* nou = (nodls*)malloc(sizeof(nodls));
+			nou->inf.nume = (char*)malloc((strlen((*cap)->inf.nume) + 1) * sizeof(char));
+			strcpy(nou->inf.nume, (*cap)->inf.nume);
+			nou->inf.nrNote = (*cap)->inf.nrNote;
+			nou->inf.note = (int*)malloc((*cap)->inf.nrNote * sizeof(int));
+			for (int i = 0; i < (*cap)->inf.nrNote; i++) {
+				nou->inf.note[i] = (*cap)->inf.note[i];
+			}
+			nou->next = NULL;
+			tempLS->next = nou;
+			tempLS = tempLS->next;
+			stergeInceputLD(cap, coada);
+		}
+	}
+	return capLS;
+
+}
+void traversareLS(nodls* cap) {
+	nodls* temp = cap;
+	while (temp != NULL) {
+		printf("\nStudentul %s", temp->inf.nume);
+		temp = temp->next;
+	}
+}
 int main()
 {
 	student a, b, c;
@@ -102,6 +183,28 @@ int main()
 	cap = inserare(cap, &coada, c);
 	traversare(cap);
 	traversareInvers(coada);
-	dezalocare(cap);
+	
+
+	//conversie lista vector
+	student* vect = NULL;
+	int nr = 0;
+	conversieVector(&cap, &vect, &nr);
+	for (int i = 0; i < nr; i++) {
+		printf("\nNume=%s ", vect[i].nume);
+	}
+	for (int i = 0; i < nr; i++) {
+		free(vect[i].nume);
+	}
+	free(vect);
+
+
+	//conversie in lista simpla
+	nodls* capLS = conversieLS(&cap, &coada);
+	printf("\nLista simpla:");
+	traversareLS(capLS);
+	if (cap == NULL) {
+		printf("\nLista dubla a fost stearsa");
+	}
+	//dezalocare(cap);
 }
 
