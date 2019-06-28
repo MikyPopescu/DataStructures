d817094b4a6c861b17a5e17cc279a676b2d9b838
Popescu Mihaela <mihaellap42@gmail.com>
2019-06-28 17:37:35 +0300

    cautare+conversie

--- Trees/Trees/Trees.cpp
+++ Trees/Trees/Trees.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include<stdio.h>
 #include<malloc.h>
+//Arbore= graf complex +aciclic
+//Arbore binar= abore care are cel mult 2 fii (are 0,1 sau 2 fii)
 //ABC Arbori
+//Arbore binar de cautare= arbore binar in care nodurile sunt asezate ai sa respecte proprietatea: stanga noduri cu valori mai mici decat nodul considerat radacina, dreapta noduri cu valori mai mari decat nodul considerat radacina
 //AVL Arbori
 struct Student {
 	int cod; //cheie
@@ -33,19 +36,19 @@ nodArb* creare(Student s, nodArb* st, nodArb* dr) {
 //inserare
 nodArb* inserare(Student s, nodArb* rad) {
 	nodArb*aux = rad;
-	if (rad == NULL)
-	{
+	if (rad == NULL)  //verific daca am unde sa inserez
+	{  //daca nu am arbore
 		aux = creare(s, NULL, NULL);
 		return aux;
 	}
-	else
+	else  //daca am arbore
 		while (true) {
-			if (s.cod < aux->inf.cod)
-				if (aux->left != NULL)
-					aux = aux->left;
+			if (s.cod < aux->inf.cod)  //trebuie sa inserez in stanga
+				if (aux->left != NULL) //daca mai sunt noduri
+					aux = aux->left;  //avansez pana ajung la frunza
 				else
 				{
-					aux->left = creare(s, NULL, NULL);
+					aux->left = creare(s, NULL, NULL); //nu mai sunt noduri asa ca pun nodul nou
 					return rad;
 				}
 			else
@@ -66,12 +69,15 @@ nodArb* inserare(Student s, nodArb* rad) {
 //parcurgeri
 void preordine(nodArb* rad) {
 	if (rad != NULL) {
+		//afisez continutul radacinii
 		printf("\nCod=%d, Nume=%s, Numar Note=%d ", rad->inf.cod, rad->inf.nume, rad->inf.nrNote);
 		for (int i = 0; i < rad->inf.nrNote; i++) {
 			printf("\nNotele: %d", rad->inf.note[i]);
 		}
 	//	printf("\nBF pentru AVL: %d", rad->BF);
+		//afisez ce am in stanga
 		preordine(rad->left);
+		//afisez ce am in dreapta
 		preordine(rad->right);
 	}
 }
@@ -111,6 +117,54 @@ void dezalocare(nodArb* rad) {
 		dezalocare(dr);
 	}
 }
+
+//inaltime
+int maxim(int a,int b) {
+	int max = a;
+	if (max < b) {
+		max = b;
+	}
+	return max;
+}
+int nrNiveluri(nodArb* rad) {
+	if (rad != NULL) {
+		return 1 + maxim(nrNiveluri(rad->left), nrNiveluri(rad->right));
+	}
+	else {
+		return 0;
+
+	}
+}
+
+//cautare
+nodArb* cautare(nodArb* rad,int cheie){
+	if (rad != NULL) {
+		if (rad->inf.cod == cheie) {
+			return rad;
+		}
+		else {
+			if (cheie < rad->inf.cod) {
+				return cautare(rad->left, cheie);
+			}
+			else {
+				return cautare(rad->right, cheie);
+			}
+		}
+	}
+	else {
+		return NULL;
+	}
+}
+
+//conversie arbore vector
+void conversieVector(nodArb* rad, Student* vect, int* nr) {
+	if (rad != NULL) {
+		vect[*nr] = rad->inf;
+		(*nr)++;
+		conversieVector(rad->left, vect, nr);
+		conversieVector(rad->right, vect, nr);
+	}
+}
 int main()
 {
 	nodArb* rad = NULL;
@@ -148,11 +202,30 @@ int main()
 	rad = inserare(b, rad);
 	rad = inserare(c, rad);
 
-
+	//parcurgere
 	inordine(rad);
 	printf("\nSubarbore stang: \n");
 	inordine(rad->left);
 	printf("\nSubarbore drept: \n");
 	inordine(rad->right);
+
+	//calcul inaltime
+	printf("\nInaltime arbore este: %d", nrNiveluri(rad));
+	
+	//cautare
+	nodArb* nodCautat = cautare(rad, 3);
+	printf("\nStudentul cautat se numeste %s", nodCautat->inf.nume);
+	
+	//vector
+	printf("\n--------------------------------------------\n");
+	Student *vect = (Student*)malloc(5 * sizeof(Student));
+	int nr = 0;
+	conversieVector(rad, vect, &nr);
+	for (int i = 0; i < nr; i++) {
+		printf("\nCod=%d, Nume=%s", vect[i].cod, vect[i].nume);
+	}
+	free(vect); //nu am copiat numele pe componente ca sa il sterg
+	
+	dezalocare(rad);
 }
 
