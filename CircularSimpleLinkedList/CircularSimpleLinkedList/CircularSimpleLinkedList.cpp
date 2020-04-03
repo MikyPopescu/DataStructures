bdece5137ebd9864db548e952bc1cb7ea2663adb
MikyPopescu <popescu9andreea17@stud.ase.ro>
2020-04-03 14:35:33 +0300

    stergere dupa nume

--- CircularSimpleLinkedList/CircularSimpleLinkedList/CircularSimpleLinkedList.cpp
+++ CircularSimpleLinkedList/CircularSimpleLinkedList/CircularSimpleLinkedList.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include<stdio.h>
 #include<string.h>
+
+
 struct Student {
 	int cod;
 	char* nume;
@@ -77,6 +79,57 @@ void dezalocare(nodls* cap) {
 	free(temp);
 }
 
+void stergereDupaNume(nodls** cap, const char* numeNou) {
+	//daca am o lista goala
+	if (*cap == NULL) {
+		return;
+	}
+	//daca am un singur nou => verific daca e cel cautat de mine
+	if (strcmp((*cap)->inf.nume, numeNou) == 0 && (*cap)->next == *cap) {
+		free((*cap)->inf.nume);
+		free((*cap)->inf.note);
+		free(*cap);
+		
+		*cap = NULL;  //problema in cazul asta??
+	}
+	
+	//Am o lista cu multe elemente si incep sa caut prin ea
+	nodls* temp = *cap;
+	nodls* deSters;
+
+	//este primul element cel pe care trebuie sa il sterg?
+	if (strcmp((*cap)->inf.nume, numeNou) == 0) { //???
+		//ma duc la finalul listei
+		while (temp->next != (*cap)) {
+			temp = temp->next;
+		}
+		temp->next = (*cap)->next;
+		free((*cap)->inf.nume);
+		free((*cap)->inf.note);
+		free(*cap);
+
+		*cap = temp->next;
+	}
+	//ma apuc sa caut elementul de sters prin lista
+	while (temp->next != (*cap) && strcmp(temp->inf.nume, numeNou) == 0) {
+		temp = temp->next;
+	}
+	//am gasit nodul de sters
+	if (strcmp(temp->inf.nume, numeNou) == 0) {
+		deSters = temp->next;
+		temp->next = deSters->next;
+		free(deSters->inf.nume);
+		free(deSters->inf.note);
+		free(deSters);
+	}
+	//nu am gasit ce sa sterg
+	else {
+		printf("\nNu s-a gasit niciun nod cu proprietatea ceruta");
+	}
+}
+
+
+
 int main()
 {
 	Student a, b, c, d;
@@ -94,11 +147,12 @@ int main()
 	b.cod = 20;
 	b.nume = (char*)malloc((strlen("Ionel") + 1) * sizeof(char));
 	strcpy(b.nume, "Ionel");
-	b.nrNote = 3;
+	b.nrNote = 4;
 	b.note = (int*)malloc(b.nrNote * sizeof(int));
 	b.note[0] = 6;
 	b.note[1] = 5;
 	b.note[2] = 5;
+	b.note[3] = 5;
 
 
 	c.cod = 30;
@@ -120,12 +174,19 @@ int main()
 
 
 
-	cap = inserare(cap, a);
+	//cap = inserare(cap, a);
 	cap = inserare(cap, b);
-	cap = inserare(cap,c);
-	cap = inserare(cap, d);
+	//cap = inserare(cap,c);
+	//cap = inserare(cap, d);
 
+	printf("\nLista dupa inserare");
 	traversare(cap);
+
+	stergereDupaNume(&cap, "Ionel");
+	printf("\nLista dupa stergere");
+	traversare(cap);
+
+
 	dezalocare(cap);
 }
 
